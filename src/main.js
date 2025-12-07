e3820abbf8767221adbbee925e563bc899077f66
ivankondev <ikondew@dev.azure.com>
2025-12-08 00:56:46 +0200

    UX: Lock Zoom & Text Inflation

--- src/main.js
+++ src/main.js
@@ -397,6 +397,22 @@ document.getElementById('btn-fullscreen').addEventListener('click', () => {
   }
 });
 
+// Prevent Pinch Zoom
+document.addEventListener('touchmove', function (event) {
+  if (event.scale !== 1) {
+    event.preventDefault();
+  }
+}, { passive: false });
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  const now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
 /* --- Event Listeners --- */
 
 document.querySelectorAll('.mode-btn').forEach(btn => {
